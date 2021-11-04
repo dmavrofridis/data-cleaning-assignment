db26e261e741bf77e4687b6a56e6d6bdcfa6c7f0
MagoriaPhantasmo <donaldbaracskay2022@u.northwestern.edu>
2021-11-04 07:46:44 -0500

    Fixed the empty row issue

--- officer_linking.py
+++ officer_linking.py
@@ -23,18 +23,29 @@ def left_join(df1, df1name, df2):
         col_names = list_with_one_out(i)
         append_data.append(pd.merge(df1, df2, left_on=col_names[0], right_on=col_names[1], how='outer'))
     vertical_stack = pd.concat(append_data)
-    # This is a list which contains the name of the columns that we have to drop when mergin tables
-    # first_name, middle_initial, last_name, suffix_name (if applicable), gender, race, appointed_date, birth year
-    dropped_columns = dropped_columns + columns_to_drop
-    vertical_stack = vertical_stack.drop(dropped_columns, axis=1)
+
+    # first, rename ids so they match the database structure
+    # then, sort by officer_id (required for next step)
+    # drop_duplicates, but only consider rows which ARE NOT officer_id, keep the first one (this is why sorting works,
+    #   the process will naturally generate a copy of officer data that is connected to a None officer_id
+    #   When we sort, Nones are pushed to the bottom.  To Keep first removes these errors
+    # drop the extra row created by our process, amusingly always assigned to Karina Aaron
     if df1name == "trr_trr_refresh":
         vertical_stack = vertical_stack.rename(columns={"id_x": "id", "id_y": "officer_id"})
         vertical_stack = vertical_stack.sort_values(by='officer_id')
         vertical_stack = vertical_stack.drop_duplicates(trr_trr_refresh_cols[:-1], keep='first')
+        vertical_stack = vertical_stack.dropna(subset=["id"])
     elif df1name == "trr_trrstatus_refresh":
         vertical_stack = vertical_stack.rename(columns={"id" : "officer_id"})
         vertical_stack = vertical_stack.sort_values(by='officer_id')
         vertical_stack = vertical_stack.drop_duplicates(trr_trrstatus_refresh_cols[:-1], keep='first')
+        vertical_stack = vertical_stack.dropna(subset=["trr_report_id"])
+
+    # This is a list which contains the name of the columns that we have to drop when mergin tables
+    # first_name, middle_initial, last_name, suffix_name (if applicable), gender, race, appointed_date, birth year
+    dropped_columns = dropped_columns + columns_to_drop
+    vertical_stack = vertical_stack.drop(dropped_columns, axis=1)
+
 
     # print(dropped_columns)
     # print(vertical_stack.columns.values.tolist())
